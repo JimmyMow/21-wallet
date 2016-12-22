39082de1130dc2a0eefaee0f2edc5be7b358608d
Jack Mallers <jackmallers@Rannys-MacBook-Pro-2.local>
2016-12-22 11:54:11 -0600

    basic routing based on whether they have a 21 wallet default.json file

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,18 +1,19 @@
 // @flow
-import os from 'os'
-import path from 'path'
-import fs from 'fs'
 import React, { Component } from 'react'
+import fs from 'fs'
 import Home from '../components/Home'
 
 export default class HomePage extends Component {
   componentWillMount() {
+    const { router } = this.props
     fs.readFile(`${process.env.HOME}/.two1/wallet/default_wallet.json`, 'utf8', (error, result) => {
       if (error) {
         console.error(error)
+        return;
       }
 
       console.log('result: ', JSON.parse(result))
+      router.push('/counter')
     })
   }
 
